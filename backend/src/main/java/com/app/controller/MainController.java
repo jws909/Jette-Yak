b6c5fd298d6facc0f73f4cad775cc24eca466254
jws909 <jws090909@gmail.com>
2026-09-17 14:28:29 +0900

    패키지 경로 오류 수정

--- backend/src/main/java/com/app/controller/MainController.java
+++ backend/src/main/java/com/app/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.app.main.controller;
+package com.app.controller;
 
 import java.util.List;
 
--- backend/src/main/java/com/app/controller/UserController.java
+++ backend/src/main/java/com/app/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.app.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
